f3378f25638d73ea43d486df13b53925304081d5
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-29 15:32:03 +0200

    search player popup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import '../common/strings.dart' as strings;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -26,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void _openSearch(BuildContext context) {
     showSearch(
       context: context,
-      delegate: PlayerSearchDelegate(_players),
+      delegate: PlayerSearchDelegate(_players, showDetailsPopup),
     );
   }
 
@@ -35,10 +33,10 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green.shade400,
-        title: const Text(strings.homeScreenTitle),
+        title: const Text('Home Screen'),
         actions: [
           IconButton(
-            onPressed: () => showDummyWorld(context),
+            onPressed: () {},
             icon: const Icon(Icons.public),
           ),
           IconButton(
@@ -49,36 +47,33 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Column(children: [
-        _players.isNotEmpty
-            ? Expanded(
-                child: ListView.builder(
-                  itemCount: _players.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      key: ValueKey(_players[index]["id"]),
-                      margin: const EdgeInsets.all(8.0),
-                      color: Colors.green.shade100,
-                      child: ListTile(
+      body: Column(
+        children: [
+          _players.isNotEmpty
+              ? Expanded(
+                  child: ListView.builder(
+                    itemCount: _players.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
                         title: Text(_players[index]["name"]),
                         subtitle: Text(_players[index]["club"]),
                         trailing: Text(_players[index]["position"]),
-                      ),
-                    );
+                      );
+                    },
+                  ),
+                )
+              : ElevatedButton(
+                  onPressed: () {
+                    readJson();
                   },
+                  child: const Center(
+                    child: Text("Load footballers"),
+                  ),
                 ),
-              )
-            : ElevatedButton(
-                onPressed: () {
-                  readJson();
-                },
-                child: const Center(
-                  child: Text("Load JSON"),
-                ),
-              ),
-      ]),
+        ],
+      ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => showDummyDialog(context),
+        onPressed: () {},
         label: const Text('Refresh'),
         icon: const Icon(Icons.restart_alt),
         backgroundColor: Colors.green,
@@ -88,28 +83,23 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void showDummyDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return const AlertDialog(
-          title: Text('Refresh'),
-        );
-      },
-    );
-  }
-
-  showDummyWorld(BuildContext context) {
-    DateTime now = DateTime.now();
-    String formattedDateTime = "${now.toLocal()}".split('.')[0];
-
+  void showDetailsPopup(
+      BuildContext context, Map<String, dynamic> playerDetails) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Location Info'),
-          content: Text(
-            'You are in Romania!\nCurrent Date and Time: $formattedDateTime',
+          title: const Text('Player Details'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Name: ${playerDetails["name"]}'),
+              Text('Club: ${playerDetails["club"]}'),
+              Text('Position: ${playerDetails["position"]}'),
+              Text('Age: ${playerDetails["age"]}'),
+              Text('Height: ${playerDetails["height"]}'),
+              Text('Foot: ${playerDetails["foot"]}'),
+            ],
           ),
         );
       },
@@ -119,8 +109,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class PlayerSearchDelegate extends SearchDelegate<String> {
   final List players;
+  final Function(BuildContext, Map<String, dynamic>) showDetailsCallback;
 
-  PlayerSearchDelegate(this.players);
+  PlayerSearchDelegate(this.players, this.showDetailsCallback);
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -158,6 +149,9 @@ class PlayerSearchDelegate extends SearchDelegate<String> {
           title: Text(searchResults[index]["name"]),
           subtitle: Text(searchResults[index]["club"]),
           trailing: Text(searchResults[index]["position"]),
+          onTap: () {
+            showDetailsCallback(context, searchResults[index]);
+          },
         );
       },
     );
@@ -178,7 +172,8 @@ class PlayerSearchDelegate extends SearchDelegate<String> {
           subtitle: Text(suggestionList[index]["club"]),
           trailing: Text(suggestionList[index]["position"]),
           onTap: () {
-            close(context, suggestionList[index]["name"]);
+            // Call the method to show details
+            showDetailsCallback(context, suggestionList[index]);
           },
         );
       },
